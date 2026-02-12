0d9eff4e5e1d8ff476495254d9fc312af4e018b6
Neeraj Patil <neeraj@Neerajs-MacBook-Pro.local>
2026-02-12 15:59:21 +0530

    completed

--- lib/core/global/initial_bindings.dart
+++ lib/core/global/initial_bindings.dart
@@ -1,11 +1,9 @@
 import 'package:prosnap/core/network/api_client.dart';
 import 'package:get/instance_manager.dart';
-import 'package:prosnap/features/auth/controllers/auth_controller.dart';
 
 class InitialBindings extends Bindings {
   @override
   void dependencies() {
     Get.put(ApiClient());
-    Get.put(AuthController());
   }
 }
