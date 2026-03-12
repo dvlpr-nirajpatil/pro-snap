63b99f71b7650ca28673fa4b4d3abfef98df7996
Neeraj Patil <neeraj@Neerajs-MacBook-Pro.local>
2026-03-12 11:51:27 +0530

    complete prosnap backend

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyDdd65dYKO4IjI_t7YWlokc9JGDIRwxbUU")
+    GMSServices.provideAPIKey("ENTER_YOUR_MAP_API_KEY")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
